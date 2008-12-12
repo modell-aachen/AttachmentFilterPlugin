09065ba7298656f53f0ff3b82c735f1ff0dc55c5
EugenMayer <EugenMayer@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-12 13:40:15 +0000

    Item441: initial release
    git-svn-id: http://svn.foswiki.org/trunk/AttachmentFilterPlugin@1294 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/Foswiki/Plugins/AttachmentFilterPlugin.pm
@@ -0,0 +1,79 @@
+#!/usr/local/bin/perl -wI.
+#
+# This script Copyright (c) 2008 Impressive.media 
+# and distributed under the GPL (see below)
+#
+# Based on parts of GenPDF, which has several sources and authors
+# This script uses html2pdf as backend, which is distributed under the LGPL
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at 
+# http://www.gnu.org/copyleft/gpl.html
+
+package Foswiki::Plugins::AttachmentFilterPlugin;
+
+use strict;
+
+use Foswiki::OopsException;
+use File::MMagic;
+use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
+
+$VERSION = '$Rev: 12445$';
+
+$RELEASE = 'Dakar';
+
+$SHORTDESCRIPTION = 'This plugin filters attachment by their content type and allows to block specific attachments';
+$NO_PREFS_IN_TOPIC = 1;
+
+$pluginName = 'AttachmentFilterPlugin';
+
+sub initPlugin {
+    my( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if( $Foswiki::Plugins::VERSION < 1.026 ) {
+        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+        return 0;
+    }
+    return 1;
+}
+
+sub beforeAttachmentSaveHandler {
+    # do not uncomment, use $_[0], $_[1]... instead
+    ###   my( $attrHashRef, $topic, $web ) = @_;
+    my $web = $_[2];
+    my $topic = $_[1];
+    my $attribs = $_[0];
+    my $mm = new File::MMagic; # use internal magic file
+    # $mm = File::MMagic->new('/etc/magic'); # use external magic file
+    #   # $mm = File::MMagic->new('/usr/share/etc/magic'); # if you use Debian
+    #     $res = $mm->checktype_filename("/somewhere/unknown/file");
+    my $fileType = $mm->checktype_filename($attribs->{'tmpFilename'});
+
+    my $allowedTypes = $Foswiki::cfg{Plugins}{AttachmentFilterPlugin}{AllowedFiltypes};
+
+    if($allowedTypes =~ m/$fileType/) {
+	# everything is ok
+    }
+    else { # forbidden filetype
+    throw Foswiki::OopsException( 'attention',
+                                       def => "dummy",
+                                       showmessage => 'You cannot upload this attachment: %PARAM1%. The type(%PARAM3%) is forbidden, please ask your administrator for further informations',
+                                       web => $web,
+                                       topic => $topic,
+				       keep => 1,
+                                       params => [ $attribs->{'attachment'},
+				                  $allowedTypes,$fileType] 
+			          );
+    }
+}
+;
+
+1;
---
+++ lib/Foswiki/Plugins/AttachmentFilterPlugin/Config.spec
@@ -0,0 +1,6 @@
+# ---+ AttachmentFilterPlugin
+# <h2>General Settings</h2>
+
+# **STRING**
+# Semicolon separated list of filetypes allowed
+$Foswiki::cfg{Plugins}{AttachmentFilterPlugin}{AllowedFiltypes}="";
