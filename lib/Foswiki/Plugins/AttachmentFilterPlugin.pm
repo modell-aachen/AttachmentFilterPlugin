99eb35b2c71030b9d9df872beefcec33550fabf1
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/AttachmentFilterPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/AttachmentFilterPlugin.pm
+++ lib/Foswiki/Plugins/AttachmentFilterPlugin.pm
@@ -1,4 +1,4 @@
-# This script Copyright (c) 2008 Impressive.media 
+# This script Copyright (c) 2008 Impressive.media
 # and distributed under the GPL (see below)
 #
 # This program is free software; you can redistribute it and/or
@@ -9,7 +9,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 
 package Foswiki::Plugins::AttachmentFilterPlugin;
@@ -18,55 +18,65 @@ use strict;
 
 use Foswiki::OopsException;
 use File::MMagic;
-use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
+use vars
+  qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
 
 $VERSION = '$Rev: 12445$';
 
 $RELEASE = 'Dakar';
 
-$SHORTDESCRIPTION = 'This plugin filters attachment by their content type and allows to block specific attachments';
+$SHORTDESCRIPTION =
+'This plugin filters attachment by their content type and allows to block specific attachments';
 $NO_PREFS_IN_TOPIC = 1;
 
 $pluginName = 'AttachmentFilterPlugin';
 
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $Foswiki::Plugins::VERSION < 1.026 ) {
-        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $Foswiki::Plugins::VERSION < 1.026 ) {
+        Foswiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
     return 1;
 }
 
 sub beforeAttachmentSaveHandler {
+
     # do not uncomment, use $_[0], $_[1]... instead
     ###   my( $attrHashRef, $topic, $web ) = @_;
-    my $web = $_[2];
-    my $topic = $_[1];
+    my $web     = $_[2];
+    my $topic   = $_[1];
     my $attribs = $_[0];
-    my $mm = new File::MMagic; # use internal magic file
-    # $mm = File::MMagic->new('/etc/magic'); # use external magic file
-    #   # $mm = File::MMagic->new('/usr/share/etc/magic'); # if you use Debian
-    #     $res = $mm->checktype_filename("/somewhere/unknown/file");
-    my $fileType = $mm->checktype_filename($attribs->{'tmpFilename'});
+    my $mm      = new File::MMagic;    # use internal magic file
+      # $mm = File::MMagic->new('/etc/magic'); # use external magic file
+      #   # $mm = File::MMagic->new('/usr/share/etc/magic'); # if you use Debian
+      #     $res = $mm->checktype_filename("/somewhere/unknown/file");
+    my $fileType = $mm->checktype_filename( $attribs->{'tmpFilename'} );
 
-    my $allowedTypes = $Foswiki::cfg{Plugins}{AttachmentFilterPlugin}{AllowedFiltypes};
+    my $allowedTypes =
+      $Foswiki::cfg{Plugins}{AttachmentFilterPlugin}{AllowedFiltypes};
 
-    if($allowedTypes =~ m/$fileType/) {
-	# everything is ok
+    if ( $allowedTypes =~ m/$fileType/ ) {
+
+        # everything is ok
     }
-    else { # forbidden filetype
-    throw Foswiki::OopsException( 'attention',
-                                       def => "generic",
-                                       web => $web,
-                                       topic => $topic,
-				       keep => 1,
-                                       params => [ 'You cannot upload this attachment: '.$attribs->{'attachment'}.". The type($fileType) is forbidden, please ask your administrator for further informations"] 
-			          );
+    else {    # forbidden filetype
+        throw Foswiki::OopsException(
+            'attention',
+            def    => "generic",
+            web    => $web,
+            topic  => $topic,
+            keep   => 1,
+            params => [
+                    'You cannot upload this attachment: '
+                  . $attribs->{'attachment'}
+                  . ". The type($fileType) is forbidden, please ask your administrator for further informations"
+            ]
+        );
     }
 }
-;
 
 1;
